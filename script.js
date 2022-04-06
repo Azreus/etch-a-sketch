05fda06491aa04a85e435d68c7f85675f318f85e
Justin <justin27sackprasit@gmail.com>
2022-04-06 20:13:19 +1000

    Rearrange Javascript sections into functions
    Rearranged certain Javascript sections and turned them into functions,
    to support compatibility with other features. Also made some changes to
    the grid's appearance.

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const mainContainer = document.querySelector(".main-container");
 
-for (let i = 0; i < 16; i++) {
-  let divRow = document.createElement("div");
-  for (let j = 0; j < 16; j++) {
-    let divCell = document.createElement("div");
-    divCell.classList.add("div-cell");
-    divRow.appendChild(divCell);
+createGrid();
+
+let divCellAll = document.querySelectorAll(".div-cell");
+
+addGridEventListener();
+
+function createGrid() {
+  for (let i = 0; i < 32; i++) {
+    let divRow = document.createElement("div");
+    for (let j = 0; j < 32; j++) {
+      let divCell = document.createElement("div");
+      divCell.classList.add("div-cell");
+      divRow.appendChild(divCell);
+    }
+    mainContainer.appendChild(divRow);
   }
-  mainContainer.appendChild(divRow);
 }
 
 // Functionality to change colours on mouseover
 
-let divCellAll = document.querySelectorAll(".div-cell");
-
-divCellAll.forEach(divCell => divCell.addEventListener("mouseover", changeColour));
+function addGridEventListener() {
+  divCellAll.forEach(divCell => divCell.addEventListener("mouseover", changeColour));
+}
 
 function changeColour(evt) {
   evt.target.style.backgroundColor = "black";
